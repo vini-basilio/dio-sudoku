538383eb5cae6609d47be57c61d85668f1af74fd
Vini <vini.ba.dev@gmail.com>
2025-03-30 19:10:08 -0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,7 @@
+import edu.dio.sudoku.BoardTemplate;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println(BoardTemplate.BOARD_TEMPLATE);
+    }
+}
